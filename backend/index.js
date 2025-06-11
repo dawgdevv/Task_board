392a68526608b8a7983f93c4533e0e834b96a4e4
dawgdevv <nraj02415@gmail.com>
2025-06-11 19:49:52 +0530

    fix: Update CORS configuration to allow credentials and modify API URL for frontend Dockerfile

--- backend/index.js
+++ backend/index.js
@@ -17,6 +17,7 @@ app.use(
     origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
   })
 );
 app.use(express.json());
